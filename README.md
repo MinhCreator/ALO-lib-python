f54f91206650dffb50a120414552f05f52a88b01
MinhCreator <buivonhatminhqwer@gmail.com>
2023-07-12 15:35:09 +0700

    all in one pack

---
+++ README.md
@@ -0,0 +1,5 @@
+# All in one package
+
+This is a simple package, it help you need nessessory function and open source. You can download 
+[Github](https://github.com/MinhCreator/ALO-lib)
+and edit your content.
---
+++ setup.py
@@ -0,0 +1,29 @@
+
+
+import setuptools
+
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='ALO package',
+    version='1.0.1',
+    description='All in one function you need',
+    author='MinhCreator VN',
+    author_email='buivonhatminhqwer@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    packages=["ALO_func"],
+    url="https://github.com/MinhCreator/ALO-lib",
+    python_requires = ">=3.7",
+    zip_safe = False,
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - released',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ])
+
+
